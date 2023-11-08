243b39e408b8186194f36d54606fcfb1dc41cc30
adn87 <aaghadieh@gmail.com>
2023-11-08 10:04:21 -0500

    NATO_Alphabet_Project_002
    - Added exception handling using: try, except, else. So when the user puts in the wrong input the program will throw an exception and ask the user to input an alphabet

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pandas
 
-
-#TODO 1. Create a dictionary in this format:
 nato_file = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_file.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("What is your name? ").upper()
-new_list = [nato_dict[letter] for letter in word]
 
-print(new_list)
+def generate_phonetic():
+    word = input("What is your name? ").upper()
+    try:
+        new_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(new_list)
 
 
+generate_phonetic()
